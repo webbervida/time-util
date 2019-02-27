b581219b716d039aefb95ff8b5f6ead9ab7e86b4
will webber <webbervida@gmail.com>
2019-02-27 13:47:31 -0600

    added tests, basic string validation, basic docs
    added tests, basic string validation, basic docs

--- src/main/java/com/ap/util/date/AddTimeUtil.java
+++ src/main/java/com/ap/util/date/AddTimeUtil.java
@@ -1,12 +1,38 @@
 package com.ap.util.date;
 
+import java.util.regex.Pattern;
+
+/**
+ * The {@code AddTimeUtil} class is a utility to add from subtract minutes on
+ * formated times.
+ *
+ * @author John Doe
+ */
 public class AddTimeUtil {
+
+	private static final Pattern pattern = Pattern.compile("([01]?[0-9]|2[0-3]):[0-5][0-9] [AP][M]");
 
 	private AddTimeUtil() {
 	}
 
-	public static String addTime(String time, int offset) {
+	/**
+	 * Returns the formatted {@code String} time with offset added or subtracted to
+	 * time passed in.
+	 *
+	 * @param time   the time in [H]H:MM {AM|PM} format.
+	 * @param offset the amount of time in minutes to add or subtract.
+	 * @return a time {@code String} that has been adjusted for the passed in number
+	 *         of offsetMinutes.
+	 * @exception IllegalArgumentException if {@code time} is in incorrect format.
+	 */
+	public static String addMinutes(final String time, final int offset) {
+		if (time == null || !isValidFormat(time)) {
+			throw new IllegalArgumentException("Time not in correct format");
+		}
 		return null;
 	}
 
+	public static boolean isValidFormat(final String time) {
+		return pattern.matcher(time).matches();
+	}
 }
--- src/test/java/com/ap/util/date/AddMinutesTest.java
+++ src/test/java/com/ap/util/date/AddMinutesTest.java
@@ -2,22 +2,46 @@ package com.ap.util.date;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class AddMinutesTest {
 
 	private final static int VALID_OFFSET = 5;
 
+	private final static String VALID_TIME = "1:15 PM";
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testNullTime() {
-		AddTimeUtil.addTime(null, VALID_OFFSET);
+		AddTimeUtil.addMinutes(null, VALID_OFFSET);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testEmptyTime() {
-		AddTimeUtil.addTime("", VALID_OFFSET);
+		AddTimeUtil.addMinutes("", VALID_OFFSET);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidTime() {
-		AddTimeUtil.addTime("ZZZ", VALID_OFFSET);
+		AddTimeUtil.addMinutes("ZZZ", VALID_OFFSET);
+	}
+
+	@Test
+	public void testNoHourRolloverNoMeridiemRollover() {
+		assertEquals("1:20 PM", AddTimeUtil.addMinutes(VALID_TIME, VALID_OFFSET));
+	}
+
+	@Test
+	public void testHourRolloverNoMeridiemRollover() {
+		assertEquals("2:20 PM", AddTimeUtil.addMinutes(VALID_TIME, VALID_OFFSET * 12));
+	}
+
+	@Test
+	public void testNoHourRolloverMeridiemRollover() {
+		assertEquals("1:15 AM", AddTimeUtil.addMinutes(VALID_TIME, VALID_OFFSET * 12 * 12));
+	}
+
+	@Test
+	public void testHourRolloverMeridiemRollover() {
+		assertEquals("2:15 AM", AddTimeUtil.addMinutes(VALID_TIME, VALID_OFFSET * 12 * 13));
 	}
 }
